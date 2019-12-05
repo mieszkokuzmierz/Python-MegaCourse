988e17fb555e46b8a327e234b42d31b8b1de1c70
Mieszko Kuźmierz <mieszko.kuzmierz@gmail.com>
2019-12-05 22:28:44 +0100

    new project

--- basics.py
+++ basics.py
@@ -1,21 +1,3 @@
-def zmiana(zdanie):
-    questions = ("how", "what", "why")
-    wielka = zdanie.capitalize()
-    if zdanie.startswith(questions):
-        return "%s?" % zdanie.format(wielka)
-    else:
-        return "%s." % zdanie.format(wielka)
-
-
-lista = []
-while True:
-    tekst = input("Say something (/end ends session): ")
-    if tekst == "/end":
-        break
-    else: lista.append(zmiana(tekst))
-
-
-print(" ".join(lista)))
-
-#adding comments to the file
+#next exercise
+print('Working with git!')
 
