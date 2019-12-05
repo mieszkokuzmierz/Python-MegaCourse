54b7fc47255da79653ed526dfda2f1ceb7504faa
Mieszko Kuźmierz <mieszko.kuzmierz@gmail.com>
2019-12-05 22:06:11 +0100

    Added initial project template

---
+++ basics.py
@@ -0,0 +1,19 @@
+def zmiana(zdanie):
+    questions = ("how", "what", "why")
+    wielka = zdanie.capitalize()
+    if zdanie.startswith(questions):
+        return "%s?" % zdanie.format(wielka)
+    else:
+        return "%s." % zdanie.format(wielka)
+
+
+lista = []
+while True:
+    tekst = input("Say something (/end ends session): ")
+    if tekst == "/end":
+        break
+    else: lista.append(zmiana(tekst))
+
+
+print(" ".join(lista)))
+
