acd7ebdf97bca6fe5391d79e3122f95f79e1d004
Mieszko Kuźmierz <mieszko.kuzmierz@gmail.com>
2019-12-19 16:21:50 +0100

    adding capital letters functionality

--- dictionary.py
+++ dictionary.py
@@ -3,15 +3,21 @@
 #Code searches through dictionary, returns its meaning, can go through minor typos and distinguish them, not giving errors
 
 import json
+from difflib import get_close_matches
 data = json.load(open("data.json"))
-
 def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data: #as there are some names like capitals in capital letters, we need to make sure it finds these as well.
+        return data[w.title()]
     else:
-        return "The word doesn't exist. Please check."
-
-word = input("Enter the word: ")
-
-print(translate(word))
+        return "The word doesn't exist. Please double check it."
+ 
+word = input("Enter word: ")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
