df3fdb5f16c9ba2cac93f4fc8e48aa1aa4a2111b
Mieszko Kuźmierz <mieszko.kuzmierz@gmail.com>
2019-12-26 20:34:08 +0100

    Adding get_close_matches functioanlity

--- dictionary.py
+++ dictionary.py
@@ -11,6 +11,18 @@ def translate(w):
         return data[w]
     elif w.title() in data: #as there are some names like capitals in capital letters, we need to make sure it finds these as well.
         return data[w.title()]
+    elif w.upper() in data: #as there are some words like USA, NATO which are all in capital letters
+        return data[w.upper()]
+    #adding get_close_matches functionality to add the words that can be found even if typo (e.g. for rainn it will ask if you meant rain instead)
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Press Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn =="N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your query."
+
     else:
         return "The word doesn't exist. Please double check it."
  
@@ -21,3 +33,4 @@ if type(output) == list:
         print(item)
 else:
     print(output)
+
