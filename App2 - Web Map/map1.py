cb85623a54d2bf72617c4e97554e8b42550cb105
Mieszko Kuźmierz <mieszko.kuzmierz@gmail.com>
2020-01-14 21:38:00 +0100

    Adding volcanoes.csv file

--- App2 - Web Map/map1.py
+++ App2 - Web Map/map1.py
@@ -1,10 +1,19 @@
 import folium
+import pandas
+import os
+
+os.chdir('/Users/zico/Dev/Mega_course/App2 - Web Map')
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My map")
 
-for coordinates in [[38.2, -99.1],[39.2, -97.1]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
