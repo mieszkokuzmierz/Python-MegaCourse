b98c45c5673db80281d6b8fd7e206092f0a03574
Mieszko Kuźmierz <mieszko.kuzmierz@gmail.com>
2020-01-17 22:53:36 +0100

    Adding elevation list to the marker.

--- App2 - Web Map/map1.py
+++ App2 - Web Map/map1.py
@@ -7,13 +7,14 @@ os.chdir('/Users/zico/Dev/Mega_course/App2 - Web Map')
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=el, icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
